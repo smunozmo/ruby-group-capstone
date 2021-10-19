df35756148cbc8136536bc01ab27c55a3cccd801
Rhyine <rhyinestewart@gmail.com>
2021-10-19 12:44:00 -0500

    Create list music album method

---
+++ music-genre/list_music_albums.rb
@@ -0,0 +1,6 @@
+def list_music_album(music_album)
+  puts '--- MUSIC ALBUM ---'
+  music_album.each do |b|
+    puts "[#{b['json_class']}] Genre: #{b['genre_name']}, Author: #{b['author_first_name']} #{b['author_last_name']}, Source: #{b['source_name']}, Label: #{b['label_title']}-#{b['label_color']}, Publish Date: #{b['item_publish_date']}, On Spotify: #{b['on_spotify']}, Moved to archived?: #{b['item_move_to_archive']}"
+  end
+end
