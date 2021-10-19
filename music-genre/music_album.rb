109808baf2ca7085d1ff0b708bd774510add37cb
Rhyine <rhyinestewart@gmail.com>
2021-10-19 12:42:17 -0500

    update reader attribute

--- music-genre/music_album.rb
+++ music-genre/music_album.rb
@@ -1,7 +1,7 @@
 require_relative '../item'
 
 class MusicAlbum < Item
-  attr_reader :on_spotify
+  attr_reader :on_spotify, :publish_date
 
   def initialize(publish_date, on_spotify)
     super(publish_date)
