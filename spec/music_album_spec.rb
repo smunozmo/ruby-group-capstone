882dc9443e3c5e41b7de4ad1781f4b74b2eff3b9
Rhyine <rhyinestewart@gmail.com>
2021-10-19 13:08:03 -0500

    Create unit test

---
+++ spec/music_album_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../music-genre/music_album'
+
+describe MusicAlbum do
+  context 'when add an album with on_spotify' do
+    it 'method move_to_archive should be true' do
+      music_album_publish_date = '2000-10-18'
+      on_spotify = true
+
+      new_music_album = MusicAlbum.new(music_album_publish_date, on_spotify)
+
+      expect(new_music_album.move_to_archive).to eq(true)
+    end
+  end
+end
