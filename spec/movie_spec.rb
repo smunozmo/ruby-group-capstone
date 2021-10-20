9e92d87d2bed895b52f41970cc1773798c94ba89
Sebastián M <smunozmo@gmail.com>
2021-10-19 19:35:59 -0500

    Add unit tests for Source and Movie classes.

---
+++ spec/movie_spec.rb
@@ -0,0 +1,36 @@
+require_relative '../movie'
+
+describe Movie do
+  context 'Movie tests' do
+    it 'Returns true if movie is silent' do
+      movie_publish_date = '2021-10-19'
+      movie_silent = true
+
+      new_movie = Movie.new(movie_publish_date, movie_silent)
+
+      expect(new_movie.move_to_archive).to eq(true)
+    end
+    
+    it 'Creates a Movie class instance' do
+        movie_publish_date = '2021-10-19'
+        movie_silent = true
+  
+        new_movie = Movie.new(movie_publish_date, movie_silent)
+  
+        expect(new_movie).to be_an_instance_of(Movie)
+    end
+
+    it 'New instance is not empty' do
+        movie_publish_date = '2021-10-19'
+        movie_silent = true
+  
+        new_movie = Movie.new(movie_publish_date, movie_silent)
+  
+        expect(new_movie.nil?).to be(false)
+    end
+
+    it 'Raises an error if no arguments are passed in' do
+        expect { new_movie = Movie.new }.to raise_error(ArgumentError)
+    end
+  end
+end
